c3a7019d7f2897a63a6f67b469269ae1198380fd
Suraj <suraj_k@tekditechnologies.com>
2023-05-18 19:42:58 +0530

    sentry integration

--- src/src/main.ts
+++ src/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const Sentry = require("@sentry/node");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
+  Sentry.init({
+    dsn: process.env.DSN_KEY,
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0.1,
+  });
   await app.listen(5000);
 }
 bootstrap();
