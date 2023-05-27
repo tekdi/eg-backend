64fd255bf4ef29e0d415ff5e70d49d3550a91cca
Tushar <tushar.mahajan@tekditechnologies.com>
2023-05-27 15:03:06 +0530

    updated subjects

--- src/src/app.module.ts
+++ src/src/app.module.ts
@@ -18,7 +18,7 @@ import { S3Module } from './services/s3/s3.module';
 import { UploadFileModule } from './upload-file/upload-file.module';
 import { UserModule } from './user.module';
 import { AttendancesModule } from './attendances/attendances.module';
-import { SubjectslistModule } from './subjectslist/subjectslist.module';
+import { SubjectsModule } from './subjects/subjects.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
@@ -40,7 +40,7 @@ import { SubjectslistModule } from './subjectslist/subjectslist.module';
     UploadFileModule,
     AttendancesModule,
     KeycloakModule,
-    SubjectslistModule,
+    SubjectsModule,
   ],
   controllers: [AppController, GeolocationController],
   providers: [AppService, GeolocationService],
