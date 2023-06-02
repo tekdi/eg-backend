401c7055db64f1607a887c908d9d8fc6abf6f347
Manoj <manoj_l@techjoomla.com>
2023-06-02 18:22:49 +0530

    Issue #000 fix: Refactoring Add module for geolocation

--- src/src/app.module.ts
+++ src/src/app.module.ts
@@ -1,15 +1,16 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { AadhaarKycModule } from 'src/modules/aadhaar_kyc/aadhaar_kyc.module';
-import { AttendancesModule } from './attendances/attendances.module';
 import { AuthModule } from 'src/modules/auth/auth.module';
+import { GeolocationModule } from 'src/modules/geolocation/geolocation.module';
+
+import { AttendancesModule } from './attendances/attendances.module';
 import { BeneficiariesModule } from './beneficiaries/beneficiaries.module';
 import { EnumModule } from './enum/enum.module';
 import { EventsModule } from './events/events.module';
 import { FacilitatorModule } from './facilitator/facilitator.module';
-import { GeolocationController } from 'src/modules/geolocation/geolocation.controller';
-import { GeolocationService } from 'src/modules/geolocation/geolocation.service';
 import { HasuraModule } from './hasura/hasura.module';
 import { HelperModule } from './helper/helper.module';
 import { InterviewModule } from './interview/interview.module';
@@ -18,31 +19,33 @@ import { S3Module } from './services/s3/s3.module';
 import { SubjectsModule } from './subjects/subjects.module';
 import { UploadFileModule } from './upload-file/upload-file.module';
 import { UserModule } from './user/user.module';
+
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    {
-      ...HttpModule.register({}),
-      global: true,
-    },
-    AadhaarKycModule,
-    AttendancesModule,
-    AuthModule,
-    BeneficiariesModule,
-    EnumModule,
-    EventsModule,
-    FacilitatorModule,
-    HasuraModule,
-    HelperModule,
-    InterviewModule,
-    KeycloakModule,
-    KeycloakModule,
-    S3Module,
-    SubjectsModule,
-    UploadFileModule,
-    UserModule,
-  ],
-  controllers: [GeolocationController],
-  providers: [GeolocationService],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		{
+			...HttpModule.register({}),
+			global: true,
+		},
+		AadhaarKycModule,
+		AttendancesModule,
+		AuthModule,
+		BeneficiariesModule,
+		EnumModule,
+		EventsModule,
+		FacilitatorModule,
+		GeolocationModule,
+		HasuraModule,
+		HelperModule,
+		InterviewModule,
+		KeycloakModule,
+		KeycloakModule,
+		S3Module,
+		SubjectsModule,
+		UploadFileModule,
+		UserModule,
+	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
