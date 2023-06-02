27994fbf1c346e4410fff73117cb7c6f6b712c01
Manoj <manoj_l@techjoomla.com>
2023-06-02 19:12:02 +0530

    Issue #000 fix: Fix sonarlint issues

--- src/src/modules/aadhaar_kyc/aadhaar_kyc.service.ts
+++ src/src/modules/aadhaar_kyc/aadhaar_kyc.service.ts
@@ -6,27 +6,26 @@ import { AxiosRequestConfig } from 'axios';
 export class AadhaarKycService {
 	constructor(private readonly httpService: HttpService) {}
 
+	private commonRequestConfig: AxiosRequestConfig = {
+		headers: {
+			'x-client-id': process.env['AADHAAR_OKYC_API_CLIENT_ID'],
+			'x-client-secret': process.env['AADHAAR_OKYC_API_CLIENT_SECRET'],
+		},
+	};
+
 	public async createOkycRequest(body, request: any, resp: any) {
 		const data = {};
 		const url = `${process.env.AADHAAR_OKYC_API_URL}`;
-		const config: AxiosRequestConfig = {
-			headers: {
-				'x-client-id': process.env['AADHAAR_OKYC_API_CLIENT_ID'],
-				'x-client-secret':
-					process.env['AADHAAR_OKYC_API_CLIENT_SECRET'],
-			},
-		};
 
 		try {
-			console.log(url);
-			console.log(data);
-			console.log(config);
-
-			const observable = this.httpService.post(url, data, config);
-
+			const observable = this.httpService.post(
+				url,
+				data,
+				this.commonRequestConfig,
+			);
 			const promise = observable.toPromise();
-
 			const response = await promise;
+
 			return resp.status(200).json({
 				success: true,
 				message: 'Created OKYC Request Successfully!',
@@ -34,6 +33,7 @@ export class AadhaarKycService {
 			});
 		} catch (e) {
 			console.log('post data error', e.message);
+
 			return resp.status(500).send({
 				success: false,
 				message: e.message,
@@ -44,18 +44,12 @@ export class AadhaarKycService {
 
 	public async initiateOkycRequest(id: any, request: any, resp: any) {
 		const url = `${process.env.AADHAAR_OKYC_API_URL}/${id}/initiate/`;
-		console.log('url', url);
-		const config: AxiosRequestConfig = {
-			headers: {
-				'x-client-id': process.env['AADHAAR_OKYC_API_CLIENT_ID'],
-				'x-client-secret':
-					process.env['AADHAAR_OKYC_API_CLIENT_SECRET'],
-				'Content-Type': 'application/json',
-			},
-		};
 
 		try {
-			const result = await this.httpService.get(url, config).toPromise();
+			const result = await this.httpService
+				.get(url, this.commonRequestConfig)
+				.toPromise();
+
 			return resp.status(200).json({
 				success: true,
 				message: 'Initiates OKYC Request Successfully!',
@@ -63,6 +57,7 @@ export class AadhaarKycService {
 			});
 		} catch (e) {
 			console.log('initiate okyc data error', e.message);
+
 			return resp.status(500).send({
 				success: false,
 				message: e.message,
@@ -79,19 +74,12 @@ export class AadhaarKycService {
 	) {
 		const url = `${process.env.AADHAAR_OKYC_API_URL}/${id}/verify/`;
 		const data = { ...body };
-		const config: AxiosRequestConfig = {
-			headers: {
-				'x-client-id': process.env['AADHAAR_OKYC_API_CLIENT_ID'],
-				'x-client-secret':
-					process.env['AADHAAR_OKYC_API_CLIENT_SECRET'],
-				'Content-Type': 'application/json',
-			},
-		};
 
 		try {
 			const result = await this.httpService
-				.post(url, data, config)
+				.post(url, data, this.commonRequestConfig)
 				.toPromise();
+
 			return resp.status(200).json({
 				success: true,
 				message: 'Verified OKYC Request Successfully!',
@@ -99,6 +87,7 @@ export class AadhaarKycService {
 			});
 		} catch (e) {
 			console.log('verify okyc data error', e.message);
+
 			return resp.status(500).send({
 				message: e.message,
 				data: {},
@@ -114,19 +103,12 @@ export class AadhaarKycService {
 	) {
 		const url = `${process.env.AADHAAR_OKYC_API_URL}/${id}/complete/`;
 		const data = { ...body };
-		const config: AxiosRequestConfig = {
-			headers: {
-				'x-client-id': process.env['AADHAAR_OKYC_API_CLIENT_ID'],
-				'x-client-secret':
-					process.env['AADHAAR_OKYC_API_CLIENT_SECRET'],
-				'Content-Type': 'application/json',
-			},
-		};
 
 		try {
 			const result = await this.httpService
-				.post(url, data, config)
+				.post(url, data, this.commonRequestConfig)
 				.toPromise();
+
 			return resp.status(200).json({
 				success: true,
 				message: 'Completed OKYC Request Successfully!',
@@ -134,6 +116,7 @@ export class AadhaarKycService {
 			});
 		} catch (e) {
 			console.log('complete okyc data error', e.message);
+
 			return resp.status(500).send({
 				message: e.message,
 				data: {},
@@ -148,24 +131,20 @@ export class AadhaarKycService {
 		resp: any,
 	) {
 		const url = `${process.env.AADHAAR_OKYC_API_URL}/${id}/${shareCode}/`;
-		const config: AxiosRequestConfig = {
-			headers: {
-				'x-client-id': process.env['AADHAAR_OKYC_API_CLIENT_ID'],
-				'x-client-secret':
-					process.env['AADHAAR_OKYC_API_CLIENT_SECRET'],
-				'Content-Type': 'application/json',
-			},
-		};
 
 		try {
-			const result = await this.httpService.get(url, config).toPromise();
+			const result = await this.httpService
+				.get(url, this.commonRequestConfig)
+				.toPromise();
+
 			return resp.status(200).json({
 				success: true,
-				message: 'Get OKYC Status Successful!',
+				message: 'Got OKYC Status Successfully!',
 				data: result.data,
 			});
 		} catch (e) {
 			console.log('get okyc data error', e.message);
+
 			return resp.status(404).send({
 				message: e.message,
 				data: {},
