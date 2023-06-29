b01c0f93daa2c55c989f9c10cc8e53eb9985f656
parthgandhitekdi <parth.gandhi@tekditechnologies.com>
2023-06-30 01:13:34 +0530

    Completed User indexing logic

--- src/src/cron/cron.service.ts
+++ src/src/cron/cron.service.ts
@@ -85,6 +85,7 @@ export class CronService {
 			}
 			return response;
 		} catch (error) {
+			console.log('disassociateAndDeleteFace:', error);
 			throw error;
 		}
 	}
@@ -93,11 +94,91 @@ export class CronService {
 		collectionId: string,
 		userId: string,
 		imageName: string,
-		externalImageId: string,
 	) {
-		// Add face in collection
-		// const indexResponse = await this.awsRekognitionService.;
-		// console.log('indexResponse:', indexResponse);
+		try {
+			// Add face in collection
+			const addFaceResponse =
+				await this.awsRekognitionService.addFaceInCollection(
+					collectionId,
+					imageName,
+				);
+
+			const response = { success: false, faceId: addFaceResponse.faceId };
+			// Associate face to user
+			if (addFaceResponse.success) {
+				const associatedPhoto = (
+					await this.awsRekognitionService.associateFaceToUser(
+						collectionId,
+						userId,
+						addFaceResponse.faceId,
+					)
+				).success;
+				if (associatedPhoto) response.success = true;
+			}
+			return response;
+		} catch (error) {
+			console.log('addAndAssociatePhotoToUser:', error);
+			throw error;
+		}
+	}
+
+	async markUserAsIndexed(
+		userId: number,
+		{ photosIndexingData, faceIdsData },
+	) {
+		let updateQuery = `
+				mutation MyMutation {
+					update_users_by_pk(
+						pk_columns: {
+							id: ${userId}
+						},
+						_set: {
+							fa_user_indexed: true,
+							fa_photos_indexed: ${JSON.stringify(photosIndexingData)},
+							fa_face_ids: ${JSON.stringify(faceIdsData)}
+						}
+					) {
+						id
+						fa_user_indexed
+						fa_photos_indexed
+						fa_face_ids
+					}
+				}
+			`;
+		try {
+			return (await this.hasuraService.getData({ query: updateQuery }))
+				.update_users_by_pk;
+		} catch (error) {
+			console.log('markUserAsIndexed:', error);
+			throw error;
+		}
+	}
+
+	async getAllUsersForAttendance() {
+		const query = `
+				query MyQuery {
+					users(where: {
+						_and: [
+							{ attendances_aggregate: {count: {predicate: {_gt: 1}}} },
+							{ attendances: { is_attendance_marked: {_is_null: false} } }
+						]
+					}) {
+						id
+						attendances(where: { is_attendance_marked: {_is_null: false} }) {
+							id
+							is_attendance_marked
+						}
+					}
+				}		  
+			`;
+		try {
+			const users = (await this.hasuraService.getData({ query }))?.data
+				?.users;
+			return users;
+		} catch (error) {
+			console.log('getAllUsersForAttendance:', error);
+			throw error;
+		}
 	}
 
 	@Cron(CronExpression.EVERY_10_SECONDS)
@@ -138,59 +219,87 @@ export class CronService {
 
 			// Step-2: Iterate through them and index faces one by one
 			for (const user of usersToIndexFaces) {
-				// Step-2.1 Fetch all faceIds of the user
+				let userId = String(user.id);
+				// Step-A Fetch all faceIds of the user
 				await this.awsRekognitionService.getAllFacesOfUser(
 					collectionId,
-					String(user.id),
+					userId,
 				);
 
-				// Step-2.2 Perform indexing of all 3 profile photos if not indexed
+				// Step-B Perform indexing of all 3 profile photos if not indexed
 				const faPhotos = JSON.parse(user.fa_photos_indexed);
 				const faFaceIds = JSON.parse(user.fa_face_ids);
 				for (let i = 1; i <= 3; i++) {
 					const photokeyName = `profile_photo_${i}`;
 					const faceIdKeyName = `faceid${i}`;
 
-					// Step-2.2.1 If photo is already then continue
+					// Step-i If photo is already then continue
 					if (faPhotos[photokeyName]) continue;
-					// Step-2.2.2 Else perform indexing based on operation
+					// Step-ii Else perform indexing based on operation
 					else {
-						// Step-2.2.2.1 Check if the photo is deleted
+						// Step-a Check if the photo is deleted
 						if (
 							(!user[photokeyName] ||
 								Object.keys(user[photokeyName]).length === 0) &&
 							faFaceIds[faceIdKeyName].trim()
 						) {
-							// Step-2.2.2.1.1 Delete photo from collection
+							// Step-a1 Delete photo from collection
 							const photoDeleted = (
 								await this.disassociateAndDeleteFace(
 									collectionId,
-									user.id,
+									userId,
 									faFaceIds[faceIdKeyName],
 								)
 							).success;
 
-							// Step-2.2.2.1.2 Set fa_face_ids.faceid(i) to null.
+							// Step-a2 Set fa_face_ids.faceid(i) to null.
 							if (photoDeleted) faFaceIds[faceIdKeyName] = null;
 
-							// Step-2.2.2.2 Check if the photo is updated
-						} else if (faFaceIds[faceIdKeyName].trim()) {
-							// Step-2.2.2.2.1 Delete photo from collection
-							const photoDeleted = (
-								await this.disassociateAndDeleteFace(
-									collectionId,
-									user.id,
-									faFaceIds[faceIdKeyName],
-								)
-							).success;
+							// Step-b Else either profile photo is newly added or updated
+						} else {
+							let addPhoto = true;
+							// Step-b1 Check if the faceId is present. If so, then profile photo is updated
+							if (faFaceIds[faceIdKeyName].trim()) {
+								// Step-b1 Delete photo from collection
+								const photoDeleted = (
+									await this.disassociateAndDeleteFace(
+										collectionId,
+										userId,
+										faFaceIds[faceIdKeyName],
+									)
+								).success;
+								addPhoto = photoDeleted;
+							}
 
-							// Step-2.2.2.2.2 Add and associate new face photo with user
-							if (photoDeleted) {
+							// Step-b2 Add and associate new face photo with user
+							if (addPhoto) {
+								const addedPhotoData =
+									await this.addAndAssociatePhotoToUser(
+										collectionId,
+										userId,
+										user[photokeyName].name,
+									);
+
+								// Step-b3 Set faceid(i) to new created faceId
+								if (addedPhotoData.success)
+									faFaceIds[faceIdKeyName] =
+										addedPhotoData.faceId;
 							}
 						}
+
+						// Step-c Set profile_photo_i to true
+						faPhotos[photokeyName] = true;
 					}
 				}
+
+				// Step-C Set user as indexed in database
+				await this.markUserAsIndexed(user.id, {
+					photosIndexingData: faPhotos,
+					faceIdsData: faFaceIds,
+				});
 			}
+
+			/*----------------------- Mark attendance -----------------------*/
 		} catch (error) {
 			// console.log();
 		}
