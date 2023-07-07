8d3f275d17a812f0116f8b82e4a2f41409e42703
parthgandhitekdi <parth.gandhi@tekditechnologies.com>
2023-07-07 14:51:48 +0530

    Uncommented mark attendance cron logic

--- src/src/cron/markAttendance.service.ts
+++ src/src/cron/markAttendance.service.ts
@@ -80,81 +80,81 @@ export class MarkAttendanceService {
 		}
 	}
 
-	// @Cron(CronExpression.EVERY_30_MINUTES)
-	// async markAttendanceCron() {
-	// 	try {
-	// 		const collectionId = this.configService.get<string>(
-	// 			'AWS_REKOGNITION_COLLECTION_ID',
-	// 		);
+	@Cron(CronExpression.EVERY_30_MINUTES)
+	async markAttendanceCron() {
+		try {
+			const collectionId = this.configService.get<string>(
+				'AWS_REKOGNITION_COLLECTION_ID',
+			);
 
-	// 		// Step-1 Fetch all users whose attendace is not marked
-	// 		const usersForAttendance = await this.getAllUsersForAttendance(
-	// 			parseInt(
-	// 				this.configService.get<string>(
-	// 					'AWS_REKOGNITION_MARK_ATTENDANCE_BATCH_SIZE',
-	// 				),
-	// 			),
-	// 		);
-	// 		console.dir(usersForAttendance, { depth: 99 });
+			// Step-1 Fetch all users whose attendace is not marked
+			const usersForAttendance = await this.getAllUsersForAttendance(
+				parseInt(
+					this.configService.get<string>(
+						'AWS_REKOGNITION_MARK_ATTENDANCE_BATCH_SIZE',
+					),
+				),
+			);
+			console.dir(usersForAttendance, { depth: 99 });
 
-	// 		// Step-2 Iterate thorugh them
-	// 		for (const user of usersForAttendance) {
-	// 			const userId = String(user.id);
-	// 			// Iterate through attendance documents and mark attendance
-	// 			for (const attendanceObj of user.attendances) {
-	// 				if (attendanceObj.photo_1) {
-	// 					// Find Users matching with image
-	// 					const matchedUser =
-	// 						await this.awsRekognitionService.searchUsersByImage(
-	// 							collectionId,
-	// 							attendanceObj.photo_1,
-	// 							parseInt(
-	// 								this.configService.get<string>(
-	// 									'AWS_REKOGNITION_FACE_MATCHING_THRESHOLD',
-	// 								),
-	// 							),
-	// 						);
-	// 					// Check if the user matched
-	// 					let matchingPercentage = null;
-	// 					const isMatchFound = matchedUser.some((obj) => {
-	// 						if (obj.User.UserId === userId) {
-	// 							matchingPercentage = obj.Similarity;
-	// 							return true;
-	// 						}
-	// 					});
-	// 					// Set attendance verified as true or false based on results
-	// 					let isAttendanceVerified = false;
-	// 					if (isMatchFound) isAttendanceVerified = true;
-	// 					console.log(
-	// 						'-------------------------------------------------------------------------',
-	// 					);
-	// 					console.log(
-	// 						`------------------------------ Verified: ${isMatchFound} ----------------------------`,
-	// 					);
-	// 					console.log(
-	// 						'-------------------------------------------------------------------------',
-	// 					);
-	// 					// Update in attendance data in database
-	// 					await this.markAttendance(attendanceObj.id, {
-	// 						isAttendanceVerified,
-	// 						matchingPercentage,
-	// 					});
-	// 					// Set delay between two attendance process
-	// 					await new Promise((resolve, reject) =>
-	// 						setTimeout(
-	// 							resolve,
-	// 							parseInt(
-	// 								this.configService.get<string>(
-	// 									'AWS_REKOGNITION_MARK_ATTENDANCE_PROCESS_INTERVAL_TIME',
-	// 								),
-	// 							),
-	// 						),
-	// 					);
-	// 				}
-	// 			}
-	// 		}
-	// 	} catch (error) {
-	// 		console.log('Error occurred in markAttendanceCron.');
-	// 	}
-	// }
+			// Step-2 Iterate thorugh them
+			for (const user of usersForAttendance) {
+				const userId = String(user.id);
+				// Iterate through attendance documents and mark attendance
+				for (const attendanceObj of user.attendances) {
+					if (attendanceObj.photo_1) {
+						// Find Users matching with image
+						const matchedUser =
+							await this.awsRekognitionService.searchUsersByImage(
+								collectionId,
+								attendanceObj.photo_1,
+								parseInt(
+									this.configService.get<string>(
+										'AWS_REKOGNITION_FACE_MATCHING_THRESHOLD',
+									),
+								),
+							);
+						// Check if the user matched
+						let matchingPercentage = null;
+						const isMatchFound = matchedUser.some((obj) => {
+							if (obj.User.UserId === userId) {
+								matchingPercentage = obj.Similarity;
+								return true;
+							}
+						});
+						// Set attendance verified as true or false based on results
+						let isAttendanceVerified = false;
+						if (isMatchFound) isAttendanceVerified = true;
+						console.log(
+							'-------------------------------------------------------------------------',
+						);
+						console.log(
+							`------------------------------ Verified: ${isMatchFound} ----------------------------`,
+						);
+						console.log(
+							'-------------------------------------------------------------------------',
+						);
+						// Update in attendance data in database
+						await this.markAttendance(attendanceObj.id, {
+							isAttendanceVerified,
+							matchingPercentage,
+						});
+						// Set delay between two attendance process
+						await new Promise((resolve, reject) =>
+							setTimeout(
+								resolve,
+								parseInt(
+									this.configService.get<string>(
+										'AWS_REKOGNITION_MARK_ATTENDANCE_PROCESS_INTERVAL_TIME',
+									),
+								),
+							),
+						);
+					}
+				}
+			}
+		} catch (error) {
+			console.log('Error occurred in markAttendanceCron.');
+		}
+	}
 }
