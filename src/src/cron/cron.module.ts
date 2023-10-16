e4dc8558a1dcf491d17eeb31647236351736a8fa
Rushi Gawali <rushi_g@tekditechnologies.com>
2023-10-16 10:42:05 +0530

    Task #200127: cron job files rename

--- src/src/cron/cron.module.ts
+++ src/src/cron/cron.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { UserCreationService } from './usercreation.service';
-import { FaceIndexingService } from './faceIndexing.service';
-import { MarkAttendanceService } from './markAttendance.service';
+import { FaUserIndexingCron } from './faUserIndexing.cron';
+import { FaFaceIndexingCron } from './faFaceIndexing.cron';
+import { FaAttendanceProcessingCron } from './faAttendanceProcessing.cron';
 import { AwsRekognitionModule } from '../services/aws-rekognition/aws-rekognition.module';
 import { HasuraModule } from '../services/hasura/hasura.module';
 @Module({
 	imports: [AwsRekognitionModule, HasuraModule],
 	providers: [
-		UserCreationService,
-		FaceIndexingService,
-		MarkAttendanceService,
+		FaUserIndexingCron,
+		FaFaceIndexingCron,
+		FaAttendanceProcessingCron,
 	],
 })
 export class CronModule {}
