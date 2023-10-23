5a80c9bf42c3ded40b2ee2a6a8b5a3c8513f2c76
Manoj L <manojLondhe@users.noreply.github.com>
2023-10-23 15:22:12 +0530

    Update sentry.service.ts

--- src/src/services/sentry/sentry.service.ts
+++ src/src/services/sentry/sentry.service.ts
@@ -5,29 +5,33 @@ import * as Sentry from '@sentry/node';
 export class SentryService {
 	async addBreadcrumb(breadcrumb: any) {
 		let objBreadcrumb = new Object({});
-		//error | debug | default
+
+		// Error | debug | default
 		if (breadcrumb?.type) {
 			objBreadcrumb['type'] = breadcrumb.type;
 		}
-		//error | info
+
+		// Error | info
 		if (breadcrumb?.level) {
 			objBreadcrumb['level'] = breadcrumb.level;
 		}
-		//must be in below format
-		//filepath.functionName
-		//e.g. cron.faUserIndexing.cron.fetchAllUsersExceptCreated
+		
+		// Must be in below format - filepath.functionName
+		// e.g. cron.faUserIndexing.cron.fetchAllUsersExceptCreated
 		if (breadcrumb?.category) {
 			objBreadcrumb['category'] = breadcrumb.category;
 		}
-		//message is description of breadcrumb
+
+		// Message is description of breadcrumb
 		if (breadcrumb?.message) {
 			objBreadcrumb['message'] = breadcrumb.message;
 		}
-		//must be in json object {}
+
+		// Must be in json object {}
 		if (breadcrumb?.data) {
 			objBreadcrumb['data'] = breadcrumb.data;
 		}
-		console.log(objBreadcrumb);
+
 		Sentry.addBreadcrumb(objBreadcrumb);
 	}
 
@@ -36,6 +40,7 @@ export class SentryService {
 			op: operation,
 			name: name,
 		});
+
 		return transaction;
 	}
 
