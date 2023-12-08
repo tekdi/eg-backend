dc043f1c3d914e13fd4112bcd3e64566af22a165
GitVivekHub <vivek.bhandurge@tekditechnologies.com>
2023-12-08 13:07:07 +0530

    dropout and rejected flow changes with respect to active learner in camp

--- src/src/beneficiaries/beneficiaries.service.ts
+++ src/src/beneficiaries/beneficiaries.service.ts
@@ -556,7 +556,7 @@ export class BeneficiariesService {
 						  ]
 								.filter((e) => e)
 								.join(' ');
-				
+
 				dataObject['user_id'] = data?.program_beneficiaries[0]?.user_id;
 				dataObject['facilitator_id'] =
 					data?.program_beneficiaries[0]?.facilitator_id;
@@ -1643,6 +1643,84 @@ export class BeneficiariesService {
 				request,
 			);
 
+		if (body?.status == 'dropout') {
+			//check if the learner is active in any camp
+
+			body.user_id = parseInt(body?.user_id);
+			let query = `query MyQuery {
+						group_users(where: {user_id: {_eq:${body?.user_id}}, status: {_eq:"active"}}){
+						  user_id
+						  status
+						  id
+						}
+					  }
+					  `;
+
+			const result = await this.hasuraServiceFromServices.getData({
+				query: query,
+			});
+
+			let group_users_data = result?.data?.group_users;
+
+			//if active learner in a camp then update its status to inactive
+
+			if (group_users_data?.length > 0) {
+				let update_body = {
+					status: 'inactive',
+				};
+
+				let group_user_id = group_users_data?.[0].id;
+				await this.hasuraService.q(
+					'group_users',
+					{
+						...update_body,
+						id: group_user_id,
+					},
+					['status'],
+					true,
+					['id', 'status'],
+				);
+			}
+		}
+
+		if (body?.status == 'rejected') {
+			//query to check if the learner is registered in a camp
+
+			let query = `query MyQuery {
+					group_users(where: {user_id: {_eq:${body?.user_id}}, status: {_eq: "active"}}) {
+					  user_id
+					  status
+					  id
+					}
+				  }
+				  `;
+			const result = await this.hasuraServiceFromServices.getData({
+				query: query,
+			});
+
+			let group_users_data = result?.data?.group_users;
+
+			//if active learner in a camp then update its status to inactive
+
+			if (group_users_data?.length > 0) {
+				let update_body = {
+					status: 'inactive',
+				};
+
+				let group_user_id = group_users_data?.[0].id;
+				await this.hasuraService.q(
+					'group_users',
+					{
+						...update_body,
+						id: group_user_id,
+					},
+					['status'],
+					true,
+					['id', 'status'],
+				);
+			}
+		}
+
 		return {
 			status: 200,
 			success: true,
@@ -1689,6 +1767,84 @@ export class BeneficiariesService {
 		}
 		const status_response =
 			await this.beneficiariesCoreService.statusUpdate(body, request);
+
+		if (body?.status == 'dropout') {
+			//check if the learner is active in any camp
+
+			body.user_id = parseInt(body?.user_id);
+			let query = `query MyQuery {
+					group_users(where: {user_id: {_eq:${body?.user_id}}, status: {_eq:"active"}}){
+					  user_id
+					  status
+					  id
+					}
+				  }
+				  `;
+
+			const result = await this.hasuraServiceFromServices.getData({
+				query: query,
+			});
+
+			let group_users_data = result?.data?.group_users;
+
+			//if active learner in a camp then update its status to inactive
+
+			if (group_users_data?.length > 0) {
+				let update_body = {
+					status: 'inactive',
+				};
+
+				let group_user_id = group_users_data?.[0].id;
+				await this.hasuraService.q(
+					'group_users',
+					{
+						...update_body,
+						id: group_user_id,
+					},
+					['status'],
+					true,
+					['id', 'status'],
+				);
+			}
+		}
+
+		if (body?.status == 'rejected') {
+			//query to check if the learner is registered in a camp
+
+			let query = `query MyQuery {
+					group_users(where: {user_id: {_eq:${body?.user_id}}, status: {_eq: "active"}}) {
+					  user_id
+					  status
+					  id
+					}
+				  }
+				  `;
+			const result = await this.hasuraServiceFromServices.getData({
+				query: query,
+			});
+
+			let group_users_data = result?.data?.group_users;
+
+			//if active learner in a camp then update its status to inactive
+
+			if (group_users_data?.length > 0) {
+				let update_body = {
+					status: 'inactive',
+				};
+
+				let group_user_id = group_users_data?.[0].id;
+				await this.hasuraService.q(
+					'group_users',
+					{
+						...update_body,
+						id: group_user_id,
+					},
+					['status'],
+					true,
+					['id', 'status'],
+				);
+			}
+		}
 
 		return {
 			status: 200,
