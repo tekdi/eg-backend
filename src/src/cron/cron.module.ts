2d7eddf5745b4cdc18e921f89ea9a143177a53bf
GitVivekHub <vivek.bhandurge@tekditechnologies.com>
2023-12-14 12:06:57 +0530

    end camp cronjob

--- src/src/cron/cron.module.ts
+++ src/src/cron/cron.module.ts
@@ -5,6 +5,7 @@ import { FaAttendanceProcessingCron } from './faAttendanceProcessing.cron';
 import { AwsRekognitionModule } from '../services/aws-rekognition/aws-rekognition.module';
 import { HasuraModule } from '../services/hasura/hasura.module';
 import { PrepareCertificateHtmlCron } from './prepareCertificateHtml.cron';
+import { CampEndCron } from './campEnd.cron';
 import { AttendancesModule } from 'src/attendances/attendances.module';
 import { UserModule } from 'src/user/user.module';
 import { Method } from 'src/common/method/method';
@@ -20,6 +21,7 @@ import { Method } from 'src/common/method/method';
 		FaFaceIndexingCron,
 		FaAttendanceProcessingCron,
 		PrepareCertificateHtmlCron,
+		CampEndCron,
 		Method,
 	],
 })
