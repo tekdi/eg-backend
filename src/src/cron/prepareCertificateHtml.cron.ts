6cf71de60b3d2302e1e54535129150ef420bed90
Rushi Gawali <rushi_g@tekditechnologies.com>
2023-12-15 19:26:57 +0530

    task #209083: changes in cron job for attendance

--- src/src/cron/prepareCertificateHtml.cron.ts
+++ src/src/cron/prepareCertificateHtml.cron.ts
@@ -24,7 +24,7 @@ export class PrepareCertificateHtmlCron {
 	) {}
 
 	//cron issue certificate run every 5 minutes
-	@Cron(CronExpression.EVERY_30_SECONDS)
+	@Cron(CronExpression.EVERY_5_MINUTES)
 	async prepareCertificateHtml() {
 		console.log('cron job: issueCertificate started at time ' + new Date());
 		//fetch all test tracking data which has certificate_status null
