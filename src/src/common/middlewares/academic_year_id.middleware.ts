7661461773fc8fc30bcc8526fe39e27220e77bdd
Manoj L <manojLondhe@users.noreply.github.com>
2023-12-29 13:06:51 +0530

    Update academic_year_id.middleware.ts

--- src/src/common/middlewares/academic_year_id.middleware.ts
+++ src/src/common/middlewares/academic_year_id.middleware.ts
@@ -14,24 +14,26 @@ export class AcademicYearIdMiddleware implements NestMiddleware {
 	async use(req: any, res: Response, next: () => void) {
 		if (req?.headers && req?.headers?.['x-academic-year-id']) {
 			req.mw_academic_year_id = req.headers['x-academic-year-id'];
+
 			// Validate format
 			const academic_year_id = parseInt(req.mw_academic_year_id, 10);
 			if (isNaN(academic_year_id)) {
 				throw new BadRequestException('Invalid academic_year_id');
 			}
+
 			// Validate access
 			const hasAccess = await this.method.isUserHasAccessForAcademicYearId(req);
 			if(!hasAccess){
 				return res.json({
 					success:false,
-					message:'User does not have access',
+					message:'User does not have access to this Academic_year Id',
 				})
 			}
 			next();
 		} else {			
 			return res.status(403).send({
 				success: false,
-				message: 'Academic year ID / Cohort ID is required',
+				message: 'Academic_year Id is required',
 			});
 		}
 	}
