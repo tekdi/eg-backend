cd0a92b3fb854b511d382fc4170cd40ee3d89964
Tushar <tushar.mahajan@tekditechnologies.com>
2024-02-02 11:43:46 +0530

    Added academic year and program id in camp by id patch api

--- src/src/camp/camp.service.ts
+++ src/src/camp/camp.service.ts
@@ -415,7 +415,7 @@ export class CampService {
 		let status = 'active';
 
 		let qury = `query MyQuery {
-			camps(where: {id:{_eq:${camp_id}},group_users: {group: {academic_year_id: {_eq:${academic_year_id}}, program_id: {_eq:${program_id}}},member_type: {_eq:"${member_type}"}, status: {_eq:"${status}"}, user_id: {_eq:${facilitator_id}}}}) {
+			camps(where: {id:{_eq:${camp_id}},group_users: {group: {academic_year_id: {_eq:${academic_year_id}}, program_id: {_eq:${program_id}}},member_type: {_eq:${member_type}}, status: {_eq:${status}}, user_id: {_eq:${facilitator_id}}}}) {
 			  id
 			  kit_ratings
 			  kit_feedback
