5cc025eb6a409e198ef1638cf932436fc1a82516
GitVivekHub <vivek.bhandurge@tekditechnologies.com>
2024-02-08 18:18:32 +0530

    okyc-date-formates-validation

--- src/src/facilitator/facilitator.core.service.ts
+++ src/src/facilitator/facilitator.core.service.ts
@@ -78,10 +78,21 @@ export class FacilitatorCoreService {
 			}
 		});
 
-		const dobFormat = 'YYYY-MM-DD';
-		if (body?.dob && !moment(body.dob, dobFormat, true).isValid()) {
-			// If the dob is not in the expected format,do not update in database
-			delete body.dob;
+		const dobFormats = ['YYYY-M-DD', 'YYYY-MM-DD', 'YYYY-M-D', 'YYYY-MM-D'];
+
+		// Check if body.dob exists and if it matches any of the expected formats
+		if (body?.dob) {
+			let validFormat = false;
+			for (const format of dobFormats) {
+				if (moment(body.dob, format, true).isValid()) {
+					validFormat = true;
+					break;
+				}
+			}
+			// If the dob is not in any of the expected formats, delete it from the database
+			if (!validFormat) {
+				delete body.dob;
+			}
 		}
 
 		const keyExist = userArr.filter((e) => Object.keys(body).includes(e));
