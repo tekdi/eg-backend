9a4e11f4e98f36102c689f27c1705aaaf5c1f354
Tusharmahajan12 <121154132+Tusharmahajan12@users.noreply.github.com>
2024-02-27 11:02:59 +0530

    Task #213168 event Enum changes and status changes for event already created  (#825)
    * Added validation in event creation
    
    * Added validation in event creation
    
    * Added validation in event creation
    
    * Added validation in event creation
    
    * Added validation in event creation
    
    * Added validation in event creation
    
    * Added validation in event creation
    
    * enum change for event
    
    * enum change for event
    
    * enum change for event
    
    * enum change for event
    
    * enum change for event
    
    * enum change for event
    
    * enum change for event

--- src/src/events/events.service.ts
+++ src/src/events/events.service.ts
@@ -133,21 +133,36 @@ export class EventsService {
 			moment(req.start_date),
 			'days',
 		);
+
+		const currentDate = moment(); // Current date
+
 		// Check if the number of attendees falls within the configured limits
-		let errorMessage = '';
+		let errorMessage = {};
+
 		const numAttendees = req.attendees.length;
 		const minParticipants = 0; // Example: Minimum number of participants allowed
 		const maxParticipants = 50; // Example: Maximum number of participants allowed
 
 		if (numAttendees < minParticipants || numAttendees > maxParticipants) {
-			errorMessage += `Number of attendees must be between ${minParticipants} and ${maxParticipants}`;
-		} else if (daysDiff < 1 || daysDiff > 5) {
-			errorMessage += 'Event duration must be between 1 and 5 days.';
+			errorMessage = {
+				key: 'participants_limit',
+				massage: `Number of attendees must be between ${minParticipants} and ${maxParticipants}`,
+			};
+		} else if (moment(req.start_date)?.isBefore(currentDate, 'day')) {
+			errorMessage = {
+				key: 'back_date',
+				message: 'start date is before the current date',
+			};
+		} else if (daysDiff < 0 || daysDiff > 5) {
+			errorMessage = {
+				key: 'event_days',
+				message: 'Event duration must be between 1 and 5 days.',
+			};
 		}
 		if (errorMessage) {
-			return response.status(200).send({
+			return response.status(422).send({
 				success: false,
-				message: errorMessage,
+				...errorMessage,
 				data: {},
 			});
 		}
@@ -169,9 +184,10 @@ export class EventsService {
 		const count = geteventData?.data?.events_aggregate?.aggregate?.count;
 		//if event created show this message
 		if (count > 0) {
-			return response.status(200).send({
-				success: true,
+			return response.status(422).send({
+				success: false,
 				message: 'Event Already created!',
+				key: 'batch_name',
 				data: {},
 			});
 		} else {
@@ -678,6 +694,7 @@ export class EventsService {
 			});
 		}
 	}
+
 	async getParticipants(req, id, body, res) {
 		const auth_users = await this.userService.ipUserInfo(req, 'staff');
 
