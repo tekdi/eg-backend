0170324be8d05fe482d26cee88c5e896b6254741
GitVivekHub <vivek.bhandurge@tekditechnologies.com>
2024-04-04 13:20:10 +0530

    Address added during prerak registration

--- src/src/modules/auth/auth.service.ts
+++ src/src/modules/auth/auth.service.ts
@@ -815,6 +815,14 @@ export class AuthService {
 				'keycloak_id',
 				'username',
 				'aadhar_verified',
+				'village',
+				'state',
+				'block',
+				'district',
+				'grampanchayat',
+				'pincode',
+				'lat',
+				'long',
 			],
 		);
 
