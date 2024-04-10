7d1eaca55be1e015c86f5e913d7b6c33af55b153
Tusharmahajan12 <121154132+Tusharmahajan12@users.noreply.github.com>
2024-04-10 16:58:18 +0530

    Bug #216977 [BE] [WEB ADMIN] : Status is getting change after refreshing the page, Add status code from API (#988)
    * Add status code
    
    * Add status code

--- src/src/camp/camp.service.ts
+++ src/src/camp/camp.service.ts
@@ -2253,6 +2253,7 @@ export class CampService {
 		}
 		if (!group_id) {
 			return resp.status(400).json({
+				status: 400,
 				message: 'CAMP_INVALID_ERROR',
 				data: [],
 			});
@@ -2275,6 +2276,7 @@ export class CampService {
 			);
 
 			return resp.status(200).json({
+				status: 200,
 				message: 'Successfully updated camp details',
 				data: camp_id,
 			});
