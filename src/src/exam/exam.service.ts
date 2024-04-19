d8a116883f675e50b3257a2720246b606ac3cb80
GitVivekHub <vivek.bhandurge@tekditechnologies.com>
2024-04-19 14:25:32 +0530

    get exam schdedule as per board id and start date

--- src/src/exam/exam.service.ts
+++ src/src/exam/exam.service.ts
@@ -239,4 +239,57 @@ export class ExamService {
 			});
 		}
 	}
+
+	async getExamScheduleByBoardIdAndDate(
+		id: any,
+		date: string,
+		resp: any,
+		request: any,
+	) {
+		let board_id = id;
+
+		let data;
+		data = {
+			query: `query MyQuery {
+				subjects(where: {board_id: {_eq: ${board_id}}}) {
+				  name
+				  id
+				  board
+				  board_id
+				  is_theory
+				  is_practical
+				  events(where: {context: {_eq: "subjects"}, start_date: {_eq: "${date}"}}) {
+					context
+					context_id
+					program_id
+					academic_year_id
+					id
+					start_date
+					end_date
+					type
+					status
+				  }
+				}
+			  }`,
+		};
+		let response = await this.hasuraServiceFromServices.queryWithVariable(
+			data,
+		);
+
+		let newQdata = response?.data?.data?.subjects;
+
+		if (newQdata?.length > 0) {
+			return resp.status(200).json({
+				success: true,
+				message: 'Data found successfully!',
+				data: newQdata,
+			});
+		} else {
+			return resp.json({
+				status: 400,
+				message: 'Data Not Found',
+				data: {},
+			});
+		}
+	}
 }
