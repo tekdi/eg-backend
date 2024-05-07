3610f30c64e9bf51dae201455233824c94188e16
Tushar <tushar.mahajan@tekditechnologies.com>
2024-05-07 10:45:12 +0530

    Create onest data

---
+++ src/src/onestusertrack/onestusertrack.controller.ts
@@ -0,0 +1,31 @@
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	Res,
+	Req,
+	UseGuards,
+	Response,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common';
+import { OnestusertrackService } from './onestusertrack.service';
+import { UpdateOnestusertrackDto } from './dto/update-onestusertrack.dto';
+import { AuthGuard } from '../modules/auth/auth.guard';
+@Controller('onestusertrack')
+export class OnestusertrackController {
+	constructor(
+		private readonly onestusertrackService: OnestusertrackService,
+	) {}
+
+	@Post('/')
+	@UsePipes(ValidationPipe)
+	@UseGuards(new AuthGuard())
+	create(@Body() body: any, @Req() request: any, @Res() response: Response) {
+		return this.onestusertrackService.create(body, request, response);
+	}
+}
