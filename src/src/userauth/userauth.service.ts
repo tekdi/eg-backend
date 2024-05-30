32fc6d003824e2451544df3b1678fc1d13b730d8
Tushar <tushar.mahajan@tekditechnologies.com>
2024-05-30 13:06:22 +0530

    Added validation and sms send

--- src/src/userauth/userauth.service.ts
+++ src/src/userauth/userauth.service.ts
@@ -1772,6 +1772,31 @@ export class UserauthService {
 	//Register volunteer
 	public async volunteerRegister(body, response, role) {
 		let misssingFieldsFlag = false;
+		const requiredFields = [
+			'first_name',
+			'middle_name',
+			'last_name',
+			'gender',
+			'mobile',
+			'email_id',
+			'dob',
+			'state',
+			'district',
+			'block',
+			'village',
+			'grampanchayat',
+			'pincode',
+			'qualification',
+		];
+
+		// Check for missing required fields
+		const missingFields = requiredFields.filter((field) => !body[field]);
+		if (missingFields.length > 0) {
+			return response.status(400).json({
+				success: false,
+				message: `Missing required fields: ${missingFields.join(', ')}`,
+			});
+		}
 		if (role === 'volunteer') {
 			//validation to check if the mobile exists for another facilitator
 
@@ -1863,6 +1888,17 @@ export class UserauthService {
 					body.role = role;
 
 					const result = await this.authService.newCreate(body);
+					// Send login details SMS
+					// नमस्कार, प्रगति प्लेटफॉर्म पर आपका अकाउंट बनाया गया है। आपका उपयोगकर्ता नाम <arg1> है और पासवर्ड <arg2> है। FEGG
+					if (body.role === 'volunteer') {
+						const message = `%E0%A4%A8%E0%A4%AE%E0%A4%B8%E0%A5%8D%E0%A4%95%E0%A4%BE%E0%A4%B0,%20%E0%A4%AA%E0%A5%8D%E0%A4%B0%E0%A4%97%E0%A4%A4%E0%A4%BF%20%E0%A4%AA%E0%A5%8D%E0%A4%B2%E0%A5%87%E0%A4%9F%E0%A4%AB%E0%A5%89%E0%A4%B0%E0%A5%8D%E0%A4%AE%20%E0%A4%AA%E0%A4%B0%20%E0%A4%86%E0%A4%AA%E0%A4%95%E0%A4%BE%20%E0%A4%85%E0%A4%95%E0%A4%BE%E0%A4%89%E0%A4%82%E0%A4%9F%20%E0%A4%AC%E0%A4%A8%E0%A4%BE%E0%A4%AF%E0%A4%BE%20%E0%A4%97%E0%A4%AF%E0%A4%BE%20%E0%A4%B9%E0%A5%88%E0%A5%A4%20%E0%A4%86%E0%A4%AA%E0%A4%95%E0%A4%BE%20%E0%A4%89%E0%A4%AA%E0%A4%AF%E0%A5%8B%E0%A4%97%E0%A4%95%E0%A4%B0%E0%A5%8D%E0%A4%A4%E0%A4%BE%20%E0%A4%A8%E0%A4%BE%E0%A4%AE%20%3Carg1%3E%20%E0%A4%B9%E0%A5%88%20%E0%A4%94%E0%A4%B0%20%E0%A4%AA%E0%A4%BE%E0%A4%B8%E0%A4%B5%E0%A4%B0%E0%A5%8D%E0%A4%A1%20%3Carg2%3E%20%E0%A4%B9%E0%A5%88%E0%A5%A4%20FEGG`;
+						const args = `arg1:${body.username},arg2:${body.password}`;
+						const otpRes = await this.authService.sendSMS(
+							body.mobile,
+							message,
+							args,
+						);
+					}
 
 					let user_id = result?.data?.id;
 					if (body.qualification) {
@@ -1930,11 +1966,6 @@ export class UserauthService {
 							context_id: user_id,
 							accepted: true,
 						};
-
-						//add acknowledgment details
-						await this.acknowledgementService.createAcknowledgement(
-							acknowledgement_create_body,
-						);
 					}
 
 					return response.status(200).send({
