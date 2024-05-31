ccda596f53fabc4778c2827f97ce85aeac32f5c9
Tusharmahajan12 <121154132+Tusharmahajan12@users.noreply.github.com>
2024-05-31 17:45:28 +0530

    Task #220278 [BE] Update volunteer data (#1179)
    * Update volunteer data
    
    * change done

--- src/src/volunteer/volunteer.service.ts
+++ src/src/volunteer/volunteer.service.ts
@@ -61,6 +61,83 @@ export class VolunteerService {
 		});
 	}
 
+	async update(id: any, body: any, request: any, resp: any) {
+		try {
+			const user_role = request?.mw_roles;
+			//Validate user role
+			if (!user_role.includes('volunteer_admin')) {
+				return resp.status(403).json({
+					success: false,
+					message: 'Permission denied. Only Volunteer Admin can See.',
+				});
+			}
+			// Check if id:volunteer and user role id is a valid ID
+			if (!id || isNaN(id) || id === 'string' || id <= 0) {
+				return resp.status(404).send({
+					success: false,
+					message: 'Invalid volunteer ID. Please provide a valid ID.',
+					data: {},
+				});
+			}
+
+			const usersFields = [
+				'first_name',
+				'last_name',
+				'gender',
+				'mobile',
+				'email_id',
+				'dob',
+				'state',
+				'pincode',
+			];
+			const userRoleUpdateFields = ['id', 'status'];
+			let userResponse = {};
+			let userRoleResponse = {};
+			if (body?.user_roles) {
+				const userRoleId = body.user_roles.id;
+				userRoleResponse = await this.hasuraService.q(
+					'user_roles',
+					{ ...body.user_roles, id: userRoleId },
+					userRoleUpdateFields,
+					true,
+					['id', 'status'],
+				);
+			}
+
+			if (body?.users) {
+				userResponse = await this.hasuraService.q(
+					'users',
+					{ ...body.users, id },
+					usersFields,
+					true,
+					[
+						'id',
+						'first_name',
+						'last_name',
+						'gender',
+						'mobile',
+						'email_id',
+						'dob',
+						'state',
+						'pincode',
+					],
+				);
+			}
+
+			return resp.status(200).json({
+				success: true,
+				message: 'Updated successfully!',
+				data: { userResponse, userRoleResponse },
+			});
+		} catch (error) {
+			return resp.status(500).json({
+				success: false,
+				message: "Couldn't update the Volunteer.",
+				data: {},
+			});
+		}
+	}
+
 	public async getVolunteerDetails(req: any, resp: any, id: any) {
 		//Validate user role
 		const user_role = req?.mw_roles;
