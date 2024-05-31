c5eac3042a28a2e2be660f1d56db7c43baf3cc81
Tushar <tushar.mahajan@tekditechnologies.com>
2024-05-31 11:01:07 +0530

    Detail API

--- src/src/volunteer/volunteer.service.ts
+++ src/src/volunteer/volunteer.service.ts
@@ -37,4 +37,72 @@ export class VolunteerService {
 			...(hasura_response || {}),
 		});
 	}
+
+	public async getVolunteerDetails(req: any, resp: any, id: any) {
+		//Validate user role
+		const user_role = req?.mw_roles;
+		if (!user_role.includes('volunteer_admin')) {
+			return resp.status(403).json({
+				success: false,
+				message: 'Permission denied. Only Volunteer Admin can See.',
+			});
+		}
+		const user_id = id;
+		try {
+			let data = {
+				query: `query MyQuery {
+          users(where: {id: {_eq: ${user_id}},user_roles:{role_slug:{_eq:"volunteer"}}}) {
+            id
+            first_name
+            middle_name
+            last_name
+            mobile
+            gender
+            dob
+            state
+            qualifications {
+              id
+              qualification_master_id
+              qualification_master {
+                name
+              }
+            }
+            user_roles {
+              id
+              user_id
+              status
+              role_slug
+            }
+          }
+        }
+        
+			`,
+			};
+
+			const response = await this.hasuraServiceFromServices.getData(data);
+
+			const volunteer = response?.data?.users || [];
+
+			if (volunteer.length == 0) {
+				return resp.status(422).send({
+					success: false,
+					message: 'volunteer Details Not found!',
+					data: volunteer,
+				});
+			} else {
+				return resp.status(200).send({
+					success: true,
+					message: 'volunteer Details found successfully!',
+					data: volunteer?.[0],
+				});
+			}
+		} catch (error) {
+			console.error('Error fetching volunteer:', error);
+			return resp.status(422).send({
+				success: false,
+				message: 'An error occurred while fetching volunteer',
+				data: {},
+			});
+		}
+	}
 }
