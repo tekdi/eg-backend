d2ca2faed0102068288f143368bcb9cf9d664368
Tushar <tushar.mahajan@tekditechnologies.com>
2024-07-08 15:19:41 +0530

    Certificate changes and remove reject and dropout learner

--- src/src/beneficiaries/beneficiaries.controller.ts
+++ src/src/beneficiaries/beneficiaries.controller.ts
@@ -291,6 +291,19 @@ export class BeneficiariesController {
 		@Res() response: any,
 		@Req() request: any,
 	) {
+		const check = await this.beneficiariesService.updateRejectDropout(
+			body,
+			request,
+		);
+
+		if (check) {
+			return response.status(check.status).json({
+				success: check.success,
+				message: check.message,
+				data: check.data,
+			});
+		}
+
 		const result = await this.beneficiariesService.statusUpdate(
 			body,
 
