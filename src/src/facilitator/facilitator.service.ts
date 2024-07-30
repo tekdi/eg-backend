42baed62d6f83f1e3ae6dded3c895ee09f9ea5ff
GitVivekHub <vivek.bhandurge@tekditechnologies.com>
2024-07-30 17:30:24 +0530

    enum changes for beneficiary status

--- src/src/facilitator/facilitator.service.ts
+++ src/src/facilitator/facilitator.service.ts
@@ -1844,7 +1844,6 @@ export class FacilitatorService {
 			variables: variables,
 		};
 
-		console.log('query--->>', data?.query);
 		let response;
 		try {
 			response = await this.hasuraService.getData(data);
