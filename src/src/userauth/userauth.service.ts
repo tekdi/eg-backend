595c69deaecb62318b1c493c0049251b4a5c5a6d
Tushar <tushar.mahajan@tekditechnologies.com>
2024-08-09 12:57:45 +0530

    check has disability

--- src/src/userauth/userauth.service.ts
+++ src/src/userauth/userauth.service.ts
@@ -18,7 +18,6 @@ const path = require('path');
 //url to base64
 import fetch from 'node-fetch';
 import { Buffer } from 'buffer';
-import { log } from 'console';
 
 @Injectable()
 export class UserauthService {
@@ -302,7 +301,6 @@ export class UserauthService {
 				};
 				//add audit logs
 				await this.userService.addAuditLogAction(auditData);
-				console.log('sss', auditData);
 
 				if (role === 'facilitator' && body?.core_faciltators) {
 					let core_faciltators = {
