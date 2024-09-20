914a8ba4a6d953fe2853e948209b06e1fb91d996
GitVivekHub <vivek.bhandurge@tekditechnologies.com>
2024-09-20 14:30:52 +0530

    Conflicts-resolved-merging-2.6.2-into-feat-sonarcloud

--- src/src/exam/exam.service.ts
+++ src/src/exam/exam.service.ts
@@ -2359,7 +2359,7 @@ export class ExamService {
 				return obj;
 			}, {} as any);
 
-		if (filteredBody.is_continued == true) {
+		if (filteredBody.is_continued) {
 			const vquery = `query MyQuery {
 		  program_beneficiaries(where: {academic_year_id: {_eq:${academic_year_id}}, program_id: {_eq:${program_id}},user_id: {_eq:${body?.user_id}},status:{_eq:"pragati_syc"}}){
 				id
